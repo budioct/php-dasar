144d2ae178507d19c11a9000359e6f9a141c0d46
Budhi Octaviansyah <budioct@gmail.com>
2024-05-10 22:52:10 +0700

    method atau function di php

---
+++ Function.php
@@ -0,0 +1,18 @@
+<?php
+
+$buat = true;
+
+if ($buat) {
+    // membuat method / function
+    function sayHello()
+    {
+        echo "Hello Function" . PHP_EOL;
+    }
+}
+
+// mengakses method / function
+sayHello();
+sayHello();
+sayHello();
+sayHello();
+sayHello();
